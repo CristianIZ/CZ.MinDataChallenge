65c50ce18debf873f922b349402f7504c31e7c94
Zappala Cristian <34014255+CristianIZ@users.noreply.github.com>
2025-04-07 08:20:15 -0300

    Mejoro pagina de inicio estandar

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, RouterModule],
+  standalone: true,
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
